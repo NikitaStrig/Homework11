64186ead87dbb4ce8364e7b306feb2fc7e4262e6
nikitastrigunkov <demonkurt1995@gmail.com>
2022-12-05 13:02:52 +0300

    HW 11 - first task

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание  1");
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
